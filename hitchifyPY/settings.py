9b39429dc3bf734cb11a04db01230be412003af1
Mykhailo Halaida <galaida.m@gmail.com>
2020-05-06 20:52:58 +0300

    reverting from heroku changes

--- hitchifyPY/settings.py
+++ hitchifyPY/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = '6l$if$n@bfycsx)r+t&#17kj3)0=px10i7bc70wq-6h*=33gpd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'hitchify.herokuapp.com'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,18 +127,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'  # this one was like this at the start
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+STATIC_URL = '/static/'
