f7f155b56e2dc33a27cd6873273966d788cf8c28
Mykhailo Halaida <galaida.m@gmail.com>
2020-05-08 01:26:53 +0300

    Basic login html, css added

--- hitchify/urls.py
+++ hitchify/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from hitchifyPY import settings
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     # path('accounts/', include('django.contrib.auth.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- hitchifyPY/settings.py
+++ hitchifyPY/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
+
+LOGIN_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
