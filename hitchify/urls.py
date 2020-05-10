0ffb581eee91b3ad8cc4afba07a0c4b494a091e4
Mykhailo Halaida <galaida.m@gmail.com>
2020-05-10 20:17:24 +0300

    URLs set up correctly

--- hitchify/urls.py
+++ hitchify/urls.py
@@ -5,6 +5,16 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='login'),
+    path('login/', views.login, name='login'),
+    path('countries/', views.countries, name='countries'),
+    path('country/<country_id>', views.country, name='country'),
+    path('hitchspots/', views.hitchspots, name='hitchspots'),
+    path('forum/', views.forum, name='forum'),
+    path('post/', views.post, name='post'),
+    path('guides/', views.guides, name='guides'),
+    path('guide/', views.guide, name='guide'),
+    path('map/', views.map, name='map'),
+
     # path('accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- hitchify/views.py
+++ hitchify/views.py
@@ -19,3 +19,83 @@ def index(request):
     }
 
     return render(request, 'index.html', context = context)
+
+def login(request):
+
+    context = {
+
+    }
+
+    return render(request, 'login.html', context = context)
+
+
+def countries(request):
+
+    country = Country.objects.all()    
+    
+    context = {
+        'country': country,
+    }
+
+    return render(request, 'countries.html', context = context)
+
+def country(request, country_id):
+
+    country = Country.objects.get(country_id=country_id)
+    
+    context = {
+        'country': country        
+    }
+
+    return render(request, 'country.html', context = context)
+
+def hitchspots(request):
+
+    hitchspot = Hitchspot.objects.all()
+
+    context = {
+        'hitchspot': hitchspot
+    }
+
+    return render(request, 'hitchspots.html', context = context)
+
+
+def forum(request):
+
+    forum = ForumPost.objects.all()
+
+    context = {
+        'forum': forum
+    }
+
+    return render(request, 'forum.html', context = context)
+
+def post(request):
+
+    context = {
+    }
+
+    return render(request, 'post.html', context = context)
+
+
+def guides(request):
+
+    guides = Guide.objects.all()
+
+    context = {
+        'guides': guides
+    }
+
+    return render(request, 'guides.html', context = context)
+
+def guide(request):
+
+    context = {}
+
+    return render(request, 'guide.html', context = context)
+
+def map(request):
+
+    context = {}
+
+    return render(request, 'map.html', context = context)
